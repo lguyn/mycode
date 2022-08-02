319a6767c950ed5903f55b30d6916226c5fb899c
leo <lguyn@users.noreply.github.com>
2022-08-02 21:30:30 +0000

    renaming and moving files

---
+++ moveplease01.py
@@ -0,0 +1,8 @@
+vim ~/mycode/moveplease01.py
+import shutil
+import os
+os.chdir('/home/student/mycode/')
+shutil.move('raynor.obj', 'ceph_storage/')
+xname = input('What is the new name for kerrigan.obj? ')
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+
