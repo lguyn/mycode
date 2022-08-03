3d3621b668fb0408765bc40c2827068cf921886f
leo <lguyn@users.noreply.github.com>
2022-08-03 17:51:49 +0000

    writing custom code

---
+++ cust_if_script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+movie= input("What movie do you want to know about?")
+
+if movie == "Harry Potter":
+    print("Good wizard fights evil wizard")
+
+elif movie == "Lord of the Rings":
+    print("Group of good guys need to destroy a ring to defeat evil")
+
+elif movie == "Lion King":
+    print("Young lion grows up and deals with evil uncle") 
+
+else:
+    print("I don't know that movie")
