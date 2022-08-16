e3d2545cb6401c3d43f70656f9848fab6cb96d85
Leo <Leo@users.noreply.github.com>
2022-08-16 16:45:09 +0000

    vars prompts

--- finalproject.py
+++ finalproject.py
@@ -16,31 +16,31 @@ def pokelist():
 def main():
     print("Welcome to the Pokedex")
     time.sleep(1)
-
     print("Search for Pokemon: Press 1\nFull list of Pokemon: Press 2\nType matchups: Press 3")
     choice=input(">")
     # asks for pokemon name then returns name, id, type, attack, and defense vertically
-    if choice== "1":
-        pokechoice= input("Choose a Pokemon:")
-        pokechoice.lower()
+    if choice == "1":
+        pokechoice.lower() = input("Choose a Pokemon:")
         with open("pokedex.txt") as pokedata:
         for x in csv.DictReader(pokedata):
         if pokechoice in x["Name"]: print(f'{x["Name"]}\n{x["#"]}\n{x["Type 1"]}\n{x["Attack"]}\n{x["Defense"]}')
+    
     # prints the 151 pokemon in gen 1
-    elif choice== "2":
+    elif choice == "2":
         for poke in pokemon["results"]:
         print(poke.get("name"))
-
     print(f"Total number of Pokemon in Generation 1: {len(pokemon['results'])}")
+    
     # is it bad to hardcode type matchups? 
-    elif choice== "3":
+    elif choice == "3":
         kind=input("What type of Pokemon would you like to know about?")
+
     # bring user back to initial prompt if request is not found
     # can I have it say "Please try again" or no because choice=input was defined above?
     else:
-        choice=input(">")
+        choice = input(">")
 
     if __name__ == "__main__":
         main()
 
-# maybe add pictures or evolution information 
+    # maybe add pictures or evolution information 
